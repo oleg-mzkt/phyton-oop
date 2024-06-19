0a719a7d098628f758cc8811c2db49ba231b27e5
Bar0 <Aleh.Kornach@gmail.com>
2024-06-19 22:26:05 +0300

    class_Users.py

--- class_Users.py
+++ class_Users.py
@@ -4,7 +4,6 @@ class Users:
     name = 'Имя'
     last_name = 'Отчество'
     id_number = 'Персональный номер'
-    phone_number = 'Номер телефона'
 
     def __init__(self,surname:str ,name:str,last_name:str,id_number:int):
         print('вызов инит')
@@ -13,10 +12,10 @@ class Users:
         self.last_name=last_name
         self.id_number=id_number
 
-    @classmethod
-    def validate(cls,arg):
-        print('ВЫЗОВ ВАЛИДЭЙТ')
-        return 7<len(cls.surname)<16
+    # @classmethod
+    # def validate(cls,arg):
+    #     print('ВЫЗОВ ВАЛИДЭЙТ')
+    #     return 7<len(cls.surname)<16
 
     def __new__(cls, *args, **kwargs):
         print('вызов для __new__ lkz '+str(cls))
@@ -26,9 +25,22 @@ class Users:
     def __del__(self):
         print('удаление экземпляра')
 
-    def set_fio(self, surname):
+    def set_User(self,surname:str ,name:str,last_name:str,id_number:int):
         self.surname = surname
-        return print(self.surname)
+        print('перезаписать данные пользователя ')
+        self.surname = surname
+        self.name = name
+        self.last_name = last_name
+        self.id_number = id_number
+        return print('сеттер для ФИО и ID записаны', )
+    def get_User(self,surname:str ,name:str,last_name:str,id_number:int):
+        self.surname = surname
+        print('перезаписать данные пользователя ')
+        self.surname = surname
+        self.name = name
+        self.last_name = last_name
+        self.id_number = id_number
+        return print('сеттер для ФИО и ID записаны', )
 
 user1=Users(input(Users.surname+':'),input(Users.name+':'),input(Users.last_name+':'),input(Users.id_number+':'))
 print('имя:',user1.surname)
