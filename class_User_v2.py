dbfb0f2e04c2dc0004b88278b7b17908b2d91e6c
oleg-mzkt <olegatimex@gmail.com>
2024-07-22 15:01:40 +0300

    Update class_User_v2.py
    добавлены деллиторы для всех свойсв

--- class_User_v2.py
+++ class_User_v2.py
@@ -7,11 +7,19 @@
 # class Interface_Person
 
 class Person:
+    """Cоздание пользователя с атрибуттами ID, ФИО, Возраст, Номер паспорта"""
     S_RUS = 'aбвгдеёжзийклмнопрстуфхцчшщьыъэюя-'
     S_RUS_UPPER = S_RUS.upper()  # заглавные буквы
-    error = '- может состоять только из русских букв и дефиса'
-    # error1='неверный ввод серии паспорта (серия паспорта должна состоять из 2ух латинских символов)'
-    error2 = 'неверный ввод номера паспорта (серия паспорта должна состоять только из 6-ти цыфр)'
+    
+    error_rus = '- может состоять только из русских букв и дефиса'
+    
+    error_year = '- год рождения может быть только цыфрами'
+    
+    error_id_number = 'ID должен состоять из 5 цыфр'
+    
+    error_passport_serial='неверный ввод серии паспорта (серия паспорта должна состоять из 2ух латинских символов)'
+    error_passport_number = 'неверный ввод номера паспорта (серия паспорта должна состоять только из 6-ти цыфр)'
+    
 
     def __init__(self, sname, name, lname, year, id_number, passport):
         self.verify_sname(sname)
@@ -28,45 +36,54 @@ class Person:
         self.__id_number = id_number
         self.__passport = passport
 
-    def __del__(self):
-        print('удаление экземпляра' + str(self))
-
     @classmethod
     def verify_sname(cls, sname):
         if type(sname) != str:
-            print(f'"{sname}" {error}')
+            print(f'"{sname}" {cls.error_rus}')
 
     @classmethod
     def verify_name(cls, name):
         error = '- может состоять только из русских букв и дефиса'
         if type(name) != str:
-            print(f'"{name}" {error}')
+            print(f'"{name}" {cls.error_rus}')
 
     @classmethod
     def verify_lname(cls, lname):
         error = '- может состоять только из русских букв и дефиса'
         if type(lname) != str:
-            print(f'"{lname}" {error}')
+            print(f'"{lname}" {cls.error_rus}')
 
     @classmethod
     def verify_year(cls, year):
-        error = '- год рождения может быть только цыфрами'
         if type(year) != int:
-            print(f'"{year}" {error}')
+            print(f'"{year}" {cls.error_year}')
 
     @classmethod
     def verify_id_number(cls, id_number):
-        error = '- идентификационный номер может быть только цыфрами'
+        error1_id_number = '- идентификационный номер должен состоять только из цыфр, 5 знаков XXXXX'
+        error2_id_number = '- идентификационный номер должен состоять из 5 цыфр'
         if type(id_number) != int:
-            print(f'"{id_number}" {error}')
+            print(f'"{id_number}" {error1_id_number}')
+        x=str(id_number)
 
-    @classmethod
+        if len(x) != 5:
+            print(f'"{id_number}" {error2_id_number}')
+
+    @classmethod # проверка паспорта корректность
     def verify_passport(cls, passport):
-        error = '- серия и номер пасспорт может быть только из первых 2ух латинских букв и 6 цыфр'
+        passport=passport.strip()
+        print(f'{passport} удаление пробелов')
+        print(f'{passport[0:2]} выделение серии')
+        print(f'{passport[2:8]} выделение номера паспорта')
         if type(passport) != str:
-            print(f'"{passport}" {error}')
+            print(f'"{passport[0:2]}" {cls.error_passport_serial}')
+        
+        if type(passport[2:6]) != str:
+            print(f'"{passport[2:6]}" {cls.error_passport_serial}')  
+        
         if len(passport) != 8:
-            print(f'"{passport}" {cls.error2}')
+            print(f'"{passport}" {cls.error_passport_number}')
+        return passport
 
     @property
     def sname(self):
@@ -76,6 +93,10 @@ class Person:
     def sname(self, sname):
         self.verify_sname(sname)
         self.__sname = sname
+    
+    @sname.deleter
+    def sname(self):
+        del (self.__sname)
 
     @property
     def name(self):
@@ -85,6 +106,10 @@ class Person:
     def name(self, name):
         self.verify_name(name)
         self.__name = name
+        
+    @name.deleter
+    def name(self):
+        del (self.__name)
 
     @property
     def lname(self):
@@ -94,6 +119,10 @@ class Person:
     def lname(self, lname):
         self.verify_name(lname)
         self.__lname = lname
+    
+    @lname.deleter
+    def lname(self):
+        del (self.__lname)
 
     @property
     def year(self):
@@ -103,6 +132,10 @@ class Person:
     def year(self, year):
         self.verify_year(year)
         self.__year = year
+    
+    @year.deleter
+    def year(self):
+        del (self.__year)
 
     @property
     def id_number(self):
@@ -112,6 +145,10 @@ class Person:
     def id_number(self, id_number):
         self.verify_id_number(id_number)
         self.__id_number = id_number
+        
+    @id_number.deleter
+    def id_number(self):
+        del (self.__id_number)
 
     @property
     def passport(self):
@@ -125,16 +162,20 @@ class Person:
     @passport.deleter
     def passport(self):
         del (self.__passport)
+        
+    # def __del__(self):
+    #     print('удаление экземпляра' + str(self))
 
 
-X = Person(sname='Корнач', name='Олег', lname='Васильевич', year=1985, id_number=23765, passport='MP1234563')
-del (X.passport)
 
-print(X.__dict__)
+X = Person(sname='Корнач', name='Олег', lname='Васильевич', year=1985, id_number=23765, passport='MP123456 ')
+#del (X.passport)
 
-Y = Person(sname='Корнач', name='Олег', lname='Васильевич', year=1985, id_number=23765, passport='MP1234563')
-print(Y.__dict__)
+#print(X.__dict__)
 
-X.id_number = 1234567890
-X.passprot = 'МР340256'
-print(X.id_number, X.passprot)
+# Y = Person(sname='Корнач', name='Олег', lname='Васильевич', year=1985, id_number=23765, passport='MP1234563')
+# print(Y.__dict__)
+
+#X.id_number = 1234567890
+#X.passprot = ' МР340256 '
+#print(X.id_number, X.passprot)
