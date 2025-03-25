ea0a621f19fa567f2f63cb06064a9c317a08e573
oleg-mzkt <olegatimex@gmail.com>
2025-03-25 15:30:13 +0300

    Create 38ex_oop_(Data Classes 2- Python).py

---
+++ 38ex_oop_(Data Classes 2- Python).py
@@ -0,0 +1,7 @@
+#38. Введение в Python Data Classes (часть 2) | Объектно-ориентированное программирование Python
+#@dataclass
+#class ThingData:
+#    name: str       
+#    weight: int     
+#    price: float = 0
+#    dims: list = field(default_factory = list)
